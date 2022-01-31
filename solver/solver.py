fecad593eeb6c0484d2d82bde60773b8bed4ee13
zhou-antong <1783908648@qq.com>
2022-01-31 23:51:18 +0800

    增加action 上锁操作剪枝无效操作

--- solver/solver.py
+++ solver/solver.py
@@ -4,11 +4,14 @@ from model.Cups import Cups
 class Solver:
     res = []
 
+    #value为1则lock 否则 unlock
+    hashlock = {}
+
     def __init__(self):
         pass
 
     @classmethod
-    def sort(cls, cups: Cups):
+    def sort(cls, cups: Cups,depth):
         if cups.is_success():
             print('solve success')
             return True
@@ -17,16 +20,28 @@ class Solver:
         for i in range(cups.cupNum):
             # j = i + 1
             for j in range(cups.cupNum):
+                if cls.hashlock.get((i, j)) == 1:
+                    continue
                 if cups.cups[i].can_pour(cups.cups[j]):
                     newcups = cups.duplicate()
                     newcups.cups[i].pour_into(newcups.cups[j])
-                    # cls.res.append((i, j))
+                    cls.res.append((i, j))
+
+                    # 解锁操作
+                    for m in range(cups.cupNum):
+                        cls.hashlock[(m, i)] = 0
+                        cls.hashlock[(i, m)] = 0
+                        cls.hashlock[(m, j)] = 0
+                        cls.hashlock[(j, m)] = 0
+
+                    # 上锁操作
+                    cls.hashlock[(j, i)] = 1
+
                     print("add " + str(i) + "," + str(j))
-                    if Solver.sort(newcups):
+                    if Solver.sort(newcups,depth+1):
                         return True
                     cls.res.pop()
                     # print("pop " + str(i) + "," + str(j))
-                j += 1
         return False
 
     @classmethod
